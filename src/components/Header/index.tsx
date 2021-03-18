e148a6ed86f3dedc263679b6d30b5d8ac1a91a7d
Vadim <Vadim_lachkov@mail.ru>
2021-03-19 02:30:30 +0700

    workhome-5 add moon, i'm forgot :)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,8 +22,8 @@ const Header = (props:IProps) =>{
     <header className={s.root}>
 
     <div className={s.forest}></div>
-            <div></div>
-            <div></div>
+            <div className={s.moon}></div>
+            <div className={s.silhouette}></div>
      <div className={s.container}>
         <h1>{title}</h1>
         <p>{descr}</p>
